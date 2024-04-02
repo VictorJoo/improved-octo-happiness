5ad5534564aa7a17d13bff574a1d996423f51cdf
Joo <sj.lim@okestro.com>
2024-04-02 16:06:02 +0900

    - 트렌드 영역 저회 및 접근 기능 추가

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -10,6 +10,7 @@ import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
 import RightSearchZone from "@/app/(afterLogin)/_component/RightSearchZone";
 import {auth} from "@/auth";
 import RQProvider from "@/app/(afterLogin)/_component/RQProvider";
+import FollowRecommendSection from "@/app/(afterLogin)/_component/FollowRecommendSection";
 
 type Props = { children: ReactNode, modal: ReactNode }
 export default async function AfterLoginLayout({children, modal}: Props) {
@@ -58,9 +59,7 @@ export default async function AfterLoginLayout({children, modal}: Props) {
                         <TrendSection/>
                         <div className={style.followRecommend}>
                             <h3>팔로우 추천</h3>
-                            <FollowRecommend/>
-                            <FollowRecommend/>
-                            <FollowRecommend/>
+                            <FollowRecommendSection />
                         </div>
                     </section>
                 </div>
